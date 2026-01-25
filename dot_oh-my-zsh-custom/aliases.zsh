b46aa4857e86d9b514f631f8b40e7f2172e322ce
drakkai <drakkai@outlook.com>
2026-01-25 17:30:26 +0100

    Update .oh-my-zsh-custom/aliases.zsh

--- dot_oh-my-zsh-custom/aliases.zsh
+++ dot_oh-my-zsh-custom/aliases.zsh
@@ -1,7 +1,1 @@
-if command -v apt > /dev/null ; then
-  alias update="sudo apt update && sudo apt upgrade -y"
-elif command -v dnf > /dev/null ; then
-  alias update="sudo dnf update -y"
-fi
-
-alias yadm_push='yadm commit -a -m "$(date +%Y-%m-%dT%T)" && yadm push origin'
+alias code="codium"
