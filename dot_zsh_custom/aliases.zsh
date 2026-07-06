4fe4ae866dbab96932ea105802081382e2c8d113
drakkai <drakkai@outlook.com>
2026-07-06 17:55:05 +0200

    Update .zsh_custom/aliases.zsh Add .zsh_custom/completions/_kubectl

--- dot_zsh_custom/aliases.zsh
+++ dot_zsh_custom/aliases.zsh
@@ -1,5 +1,10 @@
 alias code="codium"
 alias task="go-task"
 alias -- -='cd -'
+# kubectl
 alias k=kubectl
-complete -o default -F __start_kubectl k
+alias kcuc='kubectl config use-context'
+alias kcsc='kubectl config set-context'
+alias kcdc='kubectl config delete-context'
+alias kccc='kubectl config current-context'
+alias kcgc='kubectl config get-contexts'
